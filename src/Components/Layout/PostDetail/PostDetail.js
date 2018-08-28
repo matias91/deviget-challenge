260cc0e93ad32c05d3dfe98b33dd787fc7438f00
Matias Calvo <matias.calvo@sprinttech.cc>
2018-08-28 13:00:13 -0300

    Main Components

---
+++ src/Components/Layout/PostDetail/PostDetail.js
@@ -0,0 +1,21 @@
+// @Vendor
+import React, { Component } from 'react';
+
+// @Components
+// import PostsList from '../Layout/PostsList/PostsList';
+// import PostDetail from '../Layout/PostDetail/PostDetail';
+
+// @Styles
+import './PostDetail.css';
+
+class PostDetail extends Component {
+  render() {
+    return (
+      <section className="PostDetail">
+        Post Detail
+      </section>
+    );
+  }
+}
+
+export default PostDetail;
