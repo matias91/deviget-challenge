23dc2485afbae4f834c12ec70676a8e5dde1a662
Matias Calvo <matias.calvo@sprinttech.cc>
2018-08-29 11:29:20 -0300

    Post Selected Logic

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -5,16 +5,37 @@ import React, { Component } from 'react';
 import PostsList from '../Layout/PostsList/PostsList';
 import PostDetail from '../Layout/PostDetail/PostDetail';
 
+// @Data
+import Top from '../../Data/top';
+
 // @Styles
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <main className="App">
-        <PostsList />
+  constructor(props) {
+    super(props);
 
-        <PostDetail />
+    this.state = {
+      posts: Top.data.children,
+      selectedPost: null
+    }
+
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  onSelect(index) {
+    this.setState({ selectedPost: this.state.posts[index].data })
+  }
+
+  render() {
+    const { posts, selectedPost } = this.state
+    const details = selectedPost && <PostDetail post={selectedPost} />
+
+    return (
+      <main className='App'>
+        <PostsList posts={posts} selectCallback={this.onSelect} />
+
+        {details}
       </main>
     );
   }
